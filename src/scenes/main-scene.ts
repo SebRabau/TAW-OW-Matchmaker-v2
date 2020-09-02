c8d9db5ccf5191f6e2a6cf74b62f2cd1ca7dc461
Seb Rabau <strabau@gmail.com>
2020-09-03 00:06:07 +0100

    remove background for now while scaling is an issue

--- src/scenes/main-scene.ts
+++ src/scenes/main-scene.ts
@@ -17,9 +17,9 @@ export class MainScene extends Phaser.Scene
 
     protected create(): void
     {
-        this._background = this.add.image(0, 0, "background");
-        this._background.setOrigin(0, 0);
-        this._background.setDisplaySize(this.game.canvas.width, this.game.canvas.height);
+        // this._background = this.add.image(0, 0, "background");
+        // this._background.setOrigin(0, 0);
+        // this._background.setDisplaySize(this.game.canvas.width, this.game.canvas.height);
         this._logo = this.add.image(0, 0, "logo");
         this._logo.setScale(0.5, 0.5);
 
@@ -29,6 +29,6 @@ export class MainScene extends Phaser.Scene
     protected handleOrientationChanged()
     {
         this._logo.setPosition(this.game.canvas.width/2, this.game.canvas.height - this._logo.displayHeight/2);
-        this._background.setDisplaySize(this.game.canvas.width, this.game.canvas.height);
+        //this._background.setDisplaySize(this.game.canvas.width, this.game.canvas.height);
     }
 }
